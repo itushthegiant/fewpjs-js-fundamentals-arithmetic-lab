36a9e396e5124af40edac653d5efd34c7fd9cf53
etamar <etamar.levy@gmail.com>
2021-03-18 09:30:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+
+const max = Math.max(4, 20, 19);
